d64cb55a72000e41d2d1c5ad0f09890503b78ef9
x1stanzz <vova28032003@gmail.com>
2022-09-23 21:23:12 +0300

    Добавьте файлы проекта.

---
+++ Lab_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть розмір масиву: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Заповніть масив");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Елемент №{i + 1}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Масив: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+
+            Console.WriteLine();
+
+            int count = 1;
+            for (int i = 0; i < arr.Length - 1; i += count)
+            {
+                count = 1;
+                int temp = arr[i];
+                if (arr[i] == arr[i + 1])
+                    arr[i]++;
+                for (int j = 1; j < arr.Length - i; j++)
+                {
+                    if (temp == arr[i + j])
+                    {
+                        arr[i + j]++;
+                        count++;
+                    }
+                }
+            }
+
+            Console.Write("Резуьтат: ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i]);
+
+            Console.Read();
+        }
+    }
+}
